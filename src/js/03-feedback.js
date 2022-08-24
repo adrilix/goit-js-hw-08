d196a0d760e80da672c764e990195581f257eec7
Andrii Maliarenko <106884639+adrilix@users.noreply.github.com>
2022-08-24 04:27:18 +0200

    8 HW done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,47 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const formInput = document.querySelector('input');
+const formMessage = document.querySelector('textarea');
+
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
+const KEY_STORAGE = 'feedback-form-state';
+let getMeaning = null;
+findLogic();
+
+const savedValue = {
+  email: getMeaning?.email ? getMeaning.email : '',
+  message: getMeaning?.message ? getMeaning.message : '',
+};
+
+function onFormInput(event) {
+  savedValue[event.target.name] = event.target.value;
+  localStorage.setItem(KEY_STORAGE, JSON.stringify(savedValue));
+}
+
+function findLogic() {
+  getMeaning = JSON.parse(localStorage.getItem(KEY_STORAGE));
+  if (getMeaning) {
+    if (getMeaning.email) {
+      formInput.value = getMeaning.email;
+    }
+
+    if (getMeaning.message) {
+      formMessage.value = getMeaning.message;
+    }
+  }
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  if (!savedValue.email || !savedValue.message) {
+    alert('Всі рядки повинні бути заповнені');
+    return;
+  }
+  form.reset();
+  localStorage.removeItem(KEY_STORAGE);
+  console.log(savedValue);
+}
+
+// console.log(getMeaning)
